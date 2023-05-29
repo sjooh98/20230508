1d5b0a015c9e52f74f1578f05558779d65fad53b
sjooh98 <charlo1209@gmail.com>
2023-05-29 16:09:07 +0900

    render

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.sendFile('/docs/index.html');
+    res.render('/docs/index.html');
 });
 
 app.post('/api/chat', async(req, res) => {
