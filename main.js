ef1bffbad6d49851f00f611a5e4bc7b31d38f119
sjooh98 <charlo1209@gmail.com>
2023-05-29 15:39:49 +0900

    주소 바꿈

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.onload = function () {
         const prompt = document.getElementById('prompt').value;
         const responseElement = document.getElementById('response');
 
-        axios.post('http://localhost:3000/api/chat', { prompt: prompt })
+        axios.post('http://165.22.102.72:3000/api/chat', { prompt: prompt })
             .then(function (response) {
                 responseElement.textContent = response.data.choices[0].message.content;
               })
