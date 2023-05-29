6595a896e87897aedff7d87909d130e0d413fd1c
sjooh98 <charlo1209@gmail.com>
2023-05-29 16:01:56 +0900

    sendfile

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile('/docs/index.html');
 });
 
 app.post('/api/chat', async(req, res) => {
