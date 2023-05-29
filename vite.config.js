b64dc4e446761c0d85b776902b148f7ed8a23fb0
sjooh98 <charlo1209@gmail.com>
2023-05-29 16:03:40 +0900

    githubpageblock

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/20230508/',
+  base: '/',
   build: {
     // 프로젝트가 호스팅되는 GitHub Pages의 저장소 이름으로 경로를 설정합니다.
     // 예를 들어, 저장소 이름이 'my-project'라면 다음과 같이 설정합니다.
