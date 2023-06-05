0c6c750cbc04cf945f9f2630a5b87aac804eaac1
sjooh98 <charlo1209@gmail.com>
2023-06-05 15:37:32 +0900

    main, app 변경

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import axios from 'axios';
-import cors from 'cors';
-import path from 'path';
+import express from 'express'; // nodejs서버를 좀 더 쉬운 문법으로 구동시킬수 있는 프레임웤
+import axios from 'axios'; // http요청을 쉽게 할 수 있게 해주는 라이브러리 
+import cors from 'cors'; // cross origin 정책을 커스터마이징 할 수 있게 해주는 라이브러리 
+import path from 'path'; // 경로와 관련된 설정을 도와주는 라이브러리
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -9,39 +9,39 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = express();
-const PORT = 3000;
+const app = express(); // app이라는 변수에 express프레임웤을 이용해서 초기화한후, 생성된 객체를 넣어주는 코드
+const PORT = 3000; // 포트를 상수로 3000번으로 설정합니다. 
 
 app.use(express.static(path.join(__dirname, 'docs')));
-app.use(express.json());
-app.use(cors());
+app.use(express.json()); // josn이라는 형식을 쓸 수 있게 설정 
+app.use(cors()); // cors를 기본설정해서 주소가 같지 않아도 전부 통신 가능하도록 열어놓는 코드
 
 app.get('/', (req, res) => {
-    res.render('/docs/index.html');
+  res.render('/docs/index.html');
 });
 
-app.post('/api/chat', async(req, res) => {
-    const prompt = req.body.prompt;
-    try {
-        const response = await axios.post(
-            'https://api.openai.com/v1/chat/completions', 
-            {
-                "model": "gpt-3.5-turbo",
-                "messages": [{"role": "user", "content": prompt}]
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer <YOUR_API_KEY> ' 
-                }
-            }
-        );
-        res.json(response.data);
-    } catch (error) {
-        res.json({error: error.message });
-    }
+app.post('/api/chat', async (req, res) => { // post방식으로 https://localhost:3000/api/chat이라는 주소로 요청을 받는 설정
+  const prompt = req.body.prompt; // 응답(req)의 body의 prompt변수안의 값을 prompt라는 변수에 저장
+  try {
+    const response = await axios.post( // 오픈ai api 서버에 요청을 post방식으로 보냄. 그 응답을 response라는 변수에 저장
+      'https://api.openai.com/v1/chat/completions', // 해당 api 요청 주소
+      { // request body에 해당함
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": prompt}] // 배열형식 안에 role, content라는 항목을 가진 오브젝트를 넣어서 보냄
+      },
+      {
+        headers: { // 요청 헤더를 정의 
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ` // 오픈에이아이 api키를 여기에 삽입. 
+        }
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(PORT);
+  console.log(PORT);
 });
--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 import './style.css'
-import axios from 'axios';
+import axios from 'axios'; // axios 라이브러리를 가져온다. http전송을 자바스크립트 코드로 가능하게 해주는 라이브러리
 
-window.onload = function () {
-    document.getElementById('chatForm').onsubmit = function (e) {
-        e.preventDefault();
+window.onload = function () { // 자바스크립트로 접근할 수 있는 브라우저 최상단 객체가 로드되었을때, 다음 함수를 실행하라.
+  document.getElementById('chatForm').onsubmit = function (e) { // document최상단 html객체에서 'chatForm'이라는 아이디를 가진 폼객체를 찾아, onsubmit(서브밋 될뗴 실행)함수를 정의함
+    e.preventDefault(); // submit 함수의 기본 동작을 하지말라.
 
-        const prompt = document.getElementById('prompt').value;
-        const responseElement = document.getElementById('response');
+    const prompt = document.getElementById('prompt').value; // document의 'prompt'아이디를 가진 html태그를 찾아서 그 값을 const prompt라는 변수에 저장한다.
+    const responseElement = document.getElementById('response'); // document의 'response'아이디를 가진애를 찾아서 저장한다
 
-        axios.post('http://165.22.102.72:3000/api/chat', { prompt: prompt })
-            .then(function (response) {
-                responseElement.textContent = response.data.choices[0].message.content;
-              })
-            .catch(function (error) {
-                console.error('Error:', error);
-                responseElement.textContent = 'Error: ' + error.message;
-            });
-    };
-}
+    axios.post('http://157.245.154.238:3000/api/chat', { prompt: prompt }) // axios의 post방식으로 https://localhost:3000/api/chat 목적지로 prompt라는 값에 미리 정의한 prompt를 넣어서 요청한다. 
+      .then(function (response) { // 서버에서 값이 오면 
+        console.log(response);
+        responseElement.textContent = response.data.choices[0].message.content; // 출력한 태그인 p태그에 respose.data.choices[0].message.content의 텍스트 데이터를 넣어준다.  
+      })
+      .catch(function (error) { // 만약에 에러가 났을 때, 
+        console.error('Error:', error); // 에러를 콘솔에 출력하고 
+        responseElement.textContent = 'Error: ' + error.message; // 에러메시지를 출력태그의 값에 넣어서 화면에 띄워준다
+      });
+  };
+};
+
